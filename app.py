fb0ba4612256ce67bdeed907ad58dc6a90654a56
Christian Wichmann <christian@freenono.org>
2022-01-30 10:47:58 +0100

    Load categories and file data from server.

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ Sources:
 from flask import Flask, render_template, request, current_app
 
 import config
-from model import db
+from model import db, Category, CategoryType
 
 
 def init_app():
@@ -31,6 +31,8 @@ def init_app():
     db.init_app(app)
     with app.app_context():
         db.create_all()
+        from model import initializeCategories
+        initializeCategories()
         return app
 
 
@@ -42,43 +44,56 @@ def info():
     return current_app.send_static_file('main.html')
 
 
-@app.route('/categories', methods=['GET'])
+@app.route('/categories', methods=['GET', 'POST'])
 def categories():
-    return {'categories': ['Arbeitsblätter', 'Info-Texte', 'Ergebnissicherung'], 'schoolTypes': ['Grundschule', 'Realschule', 'Hauptschule', 'Berufsschule'], 'classes': ['1', '2', '3', '4'], 'subjects': ['Politik', 'Deutsch', 'Informatik', 'Englisch']}
+    categories = {'contentTypes': [c.name for c in Category.query.filter_by(type=CategoryType.contentTypes)],
+                  'schoolTypes': [c.name for c in Category.query.filter_by(type=CategoryType.schoolTypes)],
+                  'classes': [c.name for c in Category.query.filter_by(type=CategoryType.classes)],
+                  'subjects': [c.name for c in Category.query.filter_by(type=CategoryType.subjects)]}
+    if request.method == 'GET':
+        return categories
+    elif request.method == 'POST':
+        # TODO: Add category to list.
+        pass
 
 
-@app.route('/filelist', methods=['GET'])
+@app.route('/filelist', methods=['POST'])
 def list_participants():
-    categories = request.args.get('categories')
-    schoolTypes = request.args.get('schoolTypes')
-    classes = request.args.get('classes')
-    subjects = request.args.get('subjects')
-    return {'soredFiles': [
+    chosenCategories = request.json['categories']
+    print(chosenCategories)
+    allFiles =[
         {
             'filename': 'datei1.jpg',
-                        'description': 'Ein Bild eines kleinen Hundes.'
+                        'description': 'Ein Bild eines kleinen Hundes.',
+                        'categories': ['Arbeitsblätter', 'Grundschule']
         },
         {
             'filename': 'datei2.pdf',
-                        'description': 'Ein Dokument mit einer Katze. Ein Dokument mit einer Katze. Ein Dokument mit einer Katze. Ein Dokument mit einer Katze.'
+                        'description': 'Ein Dokument mit einer Katze. Ein Dokument mit einer Katze. Ein Dokument mit einer Katze. Ein Dokument mit einer Katze.',
+                        'categories': ['Info-Texte', 'Realschule']
         },
         {
             'filename': 'ein_langer_dateiname_vom_benutzer.odt',
-                        'description': 'Eine lange Datei.'
+                        'description': 'Eine lange Datei.',
+                        'categories': ['Arbeitsblätter', 'Berufsschule']
         },
         {
             'filename': 'ein_benutzer.odt',
-                        'description': 'Eine llkdfjg lsfdgjklsfjd klgjslfdk glksfd klgjlksdfg kldsfklange Datei.'
+                        'description': 'Eine llkdfjg lsfdgjklsfjd klgjslfdk glksfd klgjlksdfg kldsfklange Datei.',
+                        'categories': ['Info-Texte', 'Grundschule']
         },
         {
             'filename': 'dateiname_xyz.odt',
-                        'description': 'Eine sklgfjdgl jslkdfj gljlkfsdjg lksjdflkgj klsdfjglksdfklgj lksdfjklg lange Datei.'
+                        'description': 'Eine sklgfjdgl jslkdfj gljlkfsdjg lksjdflkgj klsdfjglksdfklgj lksdfjklg lange Datei.',
+                        'categories': ['Info-Texte', 'Berufsschule']
         },
         {
             'filename': 'Arbeitsblatt.pdf',
-                        'description': 'Eine lange Datei.'
+                        'description': 'Eine lange Datei.',
+                        'categories': ['Ergebnissicherung', 'Realschule']
         }
-    ]}
+    ]
+    return  {'storedFiles': [f for f in allFiles if chosenCategories[0] in f['categories']]}
 
 
 if __name__ == "__main__":
--- model.py
+++ model.py
@@ -1,11 +1,48 @@
 
+import enum 
+
 from flask_sqlalchemy import SQLAlchemy
 
 
 db = SQLAlchemy()
 
 
+class CategoryType(enum.Enum):
+    contentTypes = 1
+    schoolTypes = 2
+    classes = 3
+    subjects = 4
+
+FileCategory = db.Table('FileCategory',
+    db.Column('id', db.Integer, primary_key=True),
+    db.Column('fileId', db.Integer, db.ForeignKey('File.id')),
+    db.Column('categoryId', db.Integer, db.ForeignKey('Category.id')))
+
+class Category(db.Model):
+    __tablename__ = 'Category'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    type = db.Column(db.Enum(CategoryType))
+    files = db.relationship('File', secondary=FileCategory, viewonly=True)#, backref='Category'
+
 class File(db.Model):
+    __tablename__ = 'File'
     id = db.Column(db.Integer, primary_key=True)
     filename = db.Column(db.String(256), nullable=False)
-    description = db.Column(db.String(80), nullable=True)
+    description = db.Column(db.String(256), nullable=True)
+    categories = db.relationship('Category', secondary=FileCategory, viewonly=True)#, backref='File'
+
+def initializeCategories():
+    db.session.add(Category(name='Arbeitsblätter', type=CategoryType.contentTypes))
+    db.session.add(Category(name='Info-Texte', type=CategoryType.contentTypes))
+    db.session.add(Category(name='Ergebnissicherung', type=CategoryType.contentTypes))
+    db.session.add(Category(name='Grundschule', type=CategoryType.schoolTypes))
+    db.session.add(Category(name='Realschule', type=CategoryType.schoolTypes))
+    db.session.add(Category(name='Hauptschule', type=CategoryType.schoolTypes))
+    db.session.add(Category(name='Berufsschule', type=CategoryType.schoolTypes))
+    db.session.add(Category(name='Politik', type=CategoryType.subjects))
+    db.session.add(Category(name='Deutsch', type=CategoryType.subjects))
+    db.session.add(Category(name='Informatik', type=CategoryType.subjects))
+    db.session.add(Category(name='Englisch', type=CategoryType.subjects))
+    # 'classes': ['Klasse 1', 'Klasse 2', 'Klasse 3', 'Klasse 4']
+    db.session.commit()
